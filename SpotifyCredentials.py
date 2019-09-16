5ade5ddba7b74985b0f37087e0be2d6e7ec5a6ee
haran43 <haran.lakha@gmail.com>
2019-09-16 12:19:25 +0100

    Added comments to help user

--- SpotifyCredentials.py
+++ SpotifyCredentials.py
@@ -1,3 +1,4 @@
+#The authentication tokens and keys for Spotify can be found when you create an app on https://developer.spotify.com/
 CLIENT_ID = ""
 CLIENT_SECRET = ""
 scope = 'user-read-currently-playing'
