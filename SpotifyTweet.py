bf62be3aaa5d2848100f7629a7f9e8d276d1a668
haran43 <haran.lakha@gmail.com>
2019-09-17 11:53:58 +0100

    update code so no duplicate tweets are sent

--- SpotifyTweet.py
+++ SpotifyTweet.py
@@ -20,6 +20,7 @@ token = util.prompt_for_user_token(username,
 
 
 spotify = spotipy.Spotify(auth=token)
+
 while True:
     try:
         current_track = spotify.current_user_playing_track()
@@ -28,7 +29,31 @@ while True:
 
         if current_track_id != None and current_album_id != None:
             api.update_status("Currently playing: " + '\n' + current_track['item']['album']['artists'][0]['name'] + " - " + current_track['item']['name'] + '\n' +  str(current_track['item']['external_urls']['spotify']) + '\n' + "#" + str(current_track['item']['album']['artists'][0]['name']).replace(" ",""))
-       
+            break
+        else:
+            continue
+     except spotipy.client.SpotifyException:
+        token = util.prompt_for_user_token(username,
+                                scope,
+                                client_id=CLIENT_ID,
+                                client_secret=CLIENT_SECRET,
+                                redirect_uri=redirect_uri)
+
+
+        spotify = spotipy.Spotify(auth=token)
+    except (tweepy.TweepError, TypeError) as e:
+        pass
+
+
+while True:
+    try:
+        current_track1 = spotify.current_user_playing_track()
+
+        if current_track_id != None and current_album_id != None:
+            if (current_track1['item']['id'] != current_track_id or current_track1['item']['album']['id'] != current_album_id):
+                api.update_status("Currently playing: " + '\n' + current_track1['item']['album']['artists'][0]['name'] + " - " + current_track1['item']['name'] + '\n' +  str(current_track1['item']['external_urls']['spotify']) + '\n' + "#" + str(current_track1['item']['album']['artists'][0]['name']).replace(" ",""))
+                current_album_id = current_track1['item']['album']['id']
+                current_track_id = current_track1['item']['id']
         else:
             continue
     except spotipy.client.SpotifyException:
