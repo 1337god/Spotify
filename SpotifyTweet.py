86b0b324df26c990e94900c25da09fc217fba434
β︎Δℕ︎Σ β︎ℐ︎ⅅ︎ⅅ︎ℐ︎✕︎ ⛧ <1337god@cock.email>
2020-03-05 18:31:04 +0000

    Update SpotifyTweet.py

--- SpotifyTweet.py
+++ SpotifyTweet.py
@@ -3,6 +3,7 @@ import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.oauth2 as oauth2
 import tweepy
+import time
 
 from TwitterCredentials import *
 from SpotifyCredentials import *
@@ -14,6 +15,7 @@ api = tweepy.API(auth)
 
 token = util.prompt_for_user_token(username, scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=redirect_uri)
 
+
 spotify = spotipy.Spotify(auth=token)
 
 while True:
@@ -22,28 +24,51 @@ while True:
 		current_album_id = current_track['item']['album']['id']
 		current_track_id = current_track['item']['id']
 		if current_track_id != None and current_album_id != None:
-			api.update_status("Currently playing: " + '\n' + current_track['item']['album']['artists'][0]['name'] + " - " + current_track['item']['name'] + '\n' +  str(current_track['item']['external_urls']['spotify']) + '\n' + "#" + str(current_track['item']['album']['artists'][0]['name']).replace(" ",""))
+			api.update_status("#NowListening to " + current_track['item']['name'] + " by " + current_track['item']['album']['artists'][0]['name'] + '\n' +  str(current_track['item']['external_urls']['spotify']))
 			break
 		else:
+			print("Same song is playing ", current_track['item']['name'])
+			time.sleep(30)
 			continue
 	except spotipy.client.SpotifyException:
 		token = util.prompt_for_user_token(username, scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=redirect_uri)
 		spotify = spotipy.Spotify(auth=token)
+		continue
 	except (tweepy.TweepError, TypeError) as e:
-        	pass
+		if error.api_code == 187:
+			print("There's a duplicate message on Twitter.")
+		else:
+			raise error
+			pass
+	except:
+		print("No Song is playing.")
+		time.sleep(30)
+		continue
+
 
 while True:
 	try:
 		new_track = spotify.current_user_playing_track()
 		if current_track_id != None and current_album_id != None:
 			if (new_track['item']['id'] != current_track_id or new_track['item']['album']['id'] != current_album_id):
-				api.update_status("Currently playing: " + '\n' + new_track['item']['album']['artists'][0]['name'] + " - " + new_track['item']['name'] + '\n' +  str(new_track['item']['external_urls']['spotify']) + '\n' + "#" + str(new_track['item']['album']['artists'][0]['name']).replace(" ",""))
+				api.update_status("#NowListening to " + current_track['item']['name'] + " by " + current_track['item']['album']['artists'][0]['name'] + '\n' +  str(current_track['item']['external_urls']['spotify']))
 				current_album_id = new_track['item']['album']['id']
 				current_track_id = new_track['item']['id']
 		else:
+			print("Same song is playing ", current_track['item']['name'])
+			time.sleep(30)
 			continue
 	except spotipy.client.SpotifyException:
 		token = util.prompt_for_user_token(username, scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=redirect_uri)
 		spotify = spotipy.Spotify(auth=token)
-	except (tweepy.TweepError, TypeError)as e:
-		pass
+		continue
+	except (tweepy.TweepError, TypeError) as e:
+		if error.api_code == 187:
+			print("There's a duplicate message on Twitter.")
+		else:
+			raise error
+			pass
+	except:
+		print("No Song is playing.")
+		time.sleep(30)
+		continue
